fd17cdf6f5085bb900d063e41d38ddb13437539d
cortesa <cortesa@gmail.com>
2016-06-25 23:00:44 +0200

    React-Base Limpio

--- client/partials/usermenu/UserMenu.js
+++
@@ -1,9 +0,0 @@
-Template.UserMenuModal.events({
-	'click .logout': () => {
-		AccountsTemplates.logout();
-		Session.set('userMenu-toggle', '');
-	},
-	'click .userMenuItem': () => {
-		Session.set('userMenu-toggle', '');
-	}
-});
